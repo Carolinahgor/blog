9c9cec84fc6c24697e5ba764f13603f6ef8156fa
carolinahgor <carolinahgor@gmail.com>
2016-01-24 16:04:09 -0500

    adds a post model, controller and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+end
